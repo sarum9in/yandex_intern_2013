294df4212a521a4c9d7789fb8b8522cfd18bd011
Filippov Aleksey <sarum9in@gmail.com>
2013-03-28 21:21:23 +0400

    detail::radixSort() refactoring.

--- src/lib/detail/radixSort.cpp
+++ src/lib/detail/radixSort.cpp
@@ -1,5 +1,7 @@
 #include "yandex/intern/detail/radixSort.hpp"
 
+#include <boost/assert.hpp>
+
 #include <new>
 
 #include <cstring>
@@ -8,18 +10,18 @@ namespace yandex{namespace intern{namespace detail
 {
     namespace
     {
+        template <std::size_t blockSize>
         void radixSortIteration(const Data *__restrict__ const src,
                                 Data *__restrict__ const dst,
                                 const std::size_t size,
-                                const std::size_t byteShift) noexcept
+                                const std::size_t blockShift) noexcept
         {
-            constexpr std::size_t bitsInByte = 8;
-            constexpr std::uint8_t nullUChar = 0;
-            constexpr std::uint8_t fullUChar = ~nullUChar;
-            constexpr std::size_t bucketsSize = std::size_t(fullUChar) + 1;
-            constexpr Data mask = fullUChar;
+            constexpr std::size_t bitsInBlock = blockSize;
+            constexpr std::size_t fullBlock = (static_cast<std::size_t>(1) << blockSize) - 1;
+            constexpr std::size_t bucketsSize = static_cast<std::size_t>(1) << blockSize;
+            constexpr Data mask = static_cast<Data>(fullBlock);
 
-            const std::size_t bitShift = bitsInByte * byteShift;
+            const std::size_t bitShift = bitsInBlock * blockShift;
 
             std::size_t bucketCapacity[bucketsSize], bucketSize[bucketsSize];
             memset(bucketCapacity, 0, sizeof(bucketCapacity));
@@ -28,7 +30,7 @@ namespace yandex{namespace intern{namespace detail
 
             for (std::size_t i = 0; i < size; ++i)
             {
-                const std::uint8_t value = (src[i] >> bitShift) & mask;
+                const std::size_t value = (src[i] >> bitShift) & mask;
                 ++bucketCapacity[value];
             }
 
@@ -37,7 +39,7 @@ namespace yandex{namespace intern{namespace detail
 
             for (std::size_t i = 0; i < size; ++i)
             {
-                const std::uint8_t value = (src[i] >> bitShift) & mask;
+                const std::size_t value = (src[i] >> bitShift) & mask;
                 buckets[value][bucketSize[value]++] = src[i];
             }
         }
@@ -51,16 +53,19 @@ namespace yandex{namespace intern{namespace detail
         if (!buffer)
             return false;
 
+        constexpr std::size_t blockSize = 8;
+        static_assert(8 * sizeof(Data) % blockSize == 0, "");
+        constexpr std::size_t iterations = 8 * sizeof(Data) / blockSize;
+
         const Data *from = src;
-        Data *to = dst;
-        for (std::size_t byteShift = 0; byteShift < sizeof(Data); ++byteShift)
+        Data *to = iterations % 2 == 0 ? buffer : dst;
+        for (std::size_t blockShift = 0; blockShift * blockSize < 8 * sizeof(Data); ++blockShift)
         {
-            radixSortIteration(from, to, size, byteShift);
+            radixSortIteration<blockSize>(from, to, size, blockShift);
             from = to;
             to = to == buffer ? dst : buffer;
         }
-        if (from != dst) // from is old to
-            memcpy(dst, from, sizeof(to[0]) * size);
+        BOOST_ASSERT(from == dst);
         delete [] buffer;
         return true;
     }
