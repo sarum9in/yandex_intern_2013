6b31c11db9f5068fd1909aa0fcbdca1a3cfdda50
Aleksey Filippov <sarum9in@gmail.com>
2013-03-29 23:11:01 +0400

    isSorted() was moved to the main library.

---
+++ src/lib/isSorted.cpp
@@ -0,0 +1,35 @@
+#include "yandex/intern/isSorted.hpp"
+#include "yandex/intern/Error.hpp"
+#include "yandex/intern/types.hpp"
+
+#include "bunsan/enable_error_info.hpp"
+#include "bunsan/filesystem/fstream.hpp"
+
+#include <boost/assert.hpp>
+
+namespace yandex{namespace intern
+{
+    bool isSorted(const boost::filesystem::path &path)
+    {
+        BUNSAN_EXCEPTIONS_WRAP_BEGIN()
+        {
+            bunsan::filesystem::ifstream fin(path, std::ios_base::binary);
+            Data previousData;
+            fin.read(reinterpret_cast<char *>(&previousData), sizeof(previousData));
+            BOOST_ASSERT(fin.gcount() == sizeof(previousData));
+            Data data;
+            while (fin.read(reinterpret_cast<char *>(&data), sizeof(data)))
+            {
+                if (fin.gcount() != sizeof(data))
+                    BOOST_THROW_EXCEPTION(InvalidFileSizeError());
+                if (previousData > data)
+                    return false;
+                previousData = data;
+            }
+            if (fin.gcount() != 0 && fin.gcount() != sizeof(data))
+                BOOST_THROW_EXCEPTION(InvalidFileSizeError());
+        }
+        BUNSAN_EXCEPTIONS_WRAP_END_ERROR_INFO(Error::path(path))
+        return true;
+    }
+}}
