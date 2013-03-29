8cbde4aaa81223c597aaea175af4d5704c920428
Filippov Aleksey <sarum9in@gmail.com>
2013-03-29 23:47:26 +0400

    detail: radix and InMemorySorter refactoring.

--- tests/sort.cpp
+++ tests/sort.cpp
@@ -3,19 +3,68 @@
 
 #include "testSort.hpp"
 
+#include "yandex/intern/generate.hpp"
+#include "yandex/intern/isSorted.hpp"
 #include "yandex/intern/detail/radixSort.hpp"
 #include "yandex/intern/detail/stdSort.hpp"
+
+#include <boost/filesystem/operations.hpp>
 
 namespace ya = yandex::intern;
 namespace yad = ya::detail;
 
 BOOST_AUTO_TEST_SUITE(sort)
 
-BOOST_AUTO_TEST_CASE(radixSort)
+BOOST_AUTO_TEST_SUITE(radix)
+
+BOOST_AUTO_TEST_CASE(sortMemory)
 {
-    BOOST_REQUIRE(ya::test::testSort(yad::radix::sort));
-    ya::test::benchSort(yad::radix::sort, "radix::sort()");
+    BOOST_REQUIRE(ya::test::testSort(yad::radix::sortMemory));
+    ya::test::benchSort(yad::radix::sortMemory, "radix::sortMemory()");
 }
+
+struct sortFileFixture
+{
+    sortFileFixture():
+        src(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path()),
+        dst(boost::filesystem::temp_directory_path() / boost::filesystem::unique_path())
+    {
+    }
+
+    ~sortFileFixture()
+    {
+        boost::filesystem::remove(src);
+    }
+
+    void generate()
+    {
+        do
+        {
+            ya::generate(src, 1024 * 1024);
+        }
+        while(ya::isSorted(src));
+    }
+
+    const boost::filesystem::path src;
+    const boost::filesystem::path dst;
+};
+
+BOOST_FIXTURE_TEST_CASE(sortFile, sortFileFixture)
+{
+    constexpr std::size_t iterations = 100;
+    for (std::size_t i = 0; i < iterations; ++i)
+    {
+        BOOST_TEST_MESSAGE(i + 1 << "/" << iterations << " iteration");
+        generate();
+        BOOST_TEST_MESSAGE("generating...");
+        yad::radix::sortFile(src, dst);
+        BOOST_CHECK(ya::isSorted(dst));
+        yad::radix::sortFile(src, src);
+        BOOST_CHECK(ya::isSorted(src));
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END() // radix
 
 BOOST_AUTO_TEST_CASE(stdSort)
 {
