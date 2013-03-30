7e8b4e570fd214242e71054af735a6b81f4e9ef7
Aleksey Filippov <sarum9in@gmail.com>
2013-03-30 19:36:53 +0400

    detail: appendFile() and concatenate() implementation.

--- include/yandex/intern/detail/copyFile.hpp
+++ include/yandex/intern/detail/copyFile.hpp
@@ -2,7 +2,11 @@
 
 #include <boost/filesystem/path.hpp>
 
+#include <vector>
+
 namespace yandex{namespace intern{namespace detail
 {
+    void appendFile(const boost::filesystem::path &src, const int dstFd);
     void copyFile(const boost::filesystem::path &src, const boost::filesystem::path &dst);
+    void concatenate(const std::vector<boost::filesystem::path> &srcs, const boost::filesystem::path &dst);
 }}}
--- src/lib/detail/copyFile.cpp
+++ src/lib/detail/copyFile.cpp
@@ -8,11 +8,23 @@ namespace yandex{namespace intern{namespace detail
 {
     namespace unistd = yandex::contest::system::unistd;
 
-    void copyFile(const boost::filesystem::path &src, const boost::filesystem::path &dst)
+    void appendFile(const boost::filesystem::path &src, const int dstFd)
     {
         const unistd::Descriptor srcFd(unistd::open(src, O_RDONLY));
-        const unistd::Descriptor dstFd(unistd::open(dst, O_WRONLY | O_CREAT | O_TRUNC));
-        while (unistd::sendfile(dstFd.get(), srcFd.get()))
+        while (unistd::sendfile(dstFd, srcFd.get()))
             continue;
+    }
+
+    void copyFile(const boost::filesystem::path &src, const boost::filesystem::path &dst)
+    {
+        const unistd::Descriptor dstFd(unistd::open(dst, O_WRONLY | O_CREAT | O_TRUNC));
+        appendFile(src, dstFd.get());
+    }
+
+    void concatenate(const std::vector<boost::filesystem::path> &srcs, const boost::filesystem::path &dst)
+    {
+        const unistd::Descriptor dstFd(unistd::open(dst, O_WRONLY | O_CREAT | O_TRUNC));
+        for (const boost::filesystem::path &src: srcs)
+            appendFile(src, dstFd.get());
     }
 }}}
