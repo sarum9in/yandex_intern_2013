644f6b28f8645f0453351a96aaf09202427fcd6d
Aleksey Filippov <sarum9in@gmail.com>
2013-04-08 18:42:17 +0400

    balanced_split_sort, in_memory_sort and split_merge_sort CLI implementation.

---
+++ src/bin/split_merge_sort.cpp
@@ -0,0 +1,22 @@
+#include "yandex/intern/sorters/SplitMergeSorter.hpp"
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    if (argc != 2 + 1)
+    {
+        std::cerr << "Usage: " << argv[0] << " ${src} ${dst}" << std::endl;
+        return 2;
+    }
+    try
+    {
+        yandex::intern::sort<yandex::intern::sorters::SplitMergeSorter>(argv[1], argv[2]);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error occurred: " << e.what() << std::endl;
+        return 1;
+    }
+}
