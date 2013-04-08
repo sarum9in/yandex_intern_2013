116f37de529ac6c931a3d911380efface884759f
Aleksey Filippov <sarum9in@gmail.com>
2013-04-08 14:06:16 +0400

    sorters::BalancedSplitSorter refactoring.

--- include/yandex/intern/sorters/BalancedSplitSorter.hpp
+++ include/yandex/intern/sorters/BalancedSplitSorter.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "yandex/intern/Sorter.hpp"
+#include "yandex/intern/types.hpp"
+
+#include <unordered_map>
+#include <vector>
 
 namespace yandex{namespace intern{namespace sorters
 {
@@ -14,6 +18,16 @@ namespace yandex{namespace intern{namespace sorters
         void sort() override;
 
     private:
+        void buildPrefixSplit();
+        void buildCompressedPrefixSplit(const std::size_t prefix);
+
+    private:
         const boost::filesystem::path root_;
+        std::size_t inputByteSize_;
+        /// prefix is modified Data, it may be bigger at first steps
+        std::unordered_map<std::size_t, std::size_t> prefix2id_;
+        std::vector<Data> id2prefix_;
+        std::vector<std::size_t> id2size_;
+        std::vector<bool> isEnd_;
     };
 }}}
--- src/lib/sorters/BalancedSplitSorter.cpp
+++ src/lib/sorters/BalancedSplitSorter.cpp
@@ -22,6 +22,15 @@ namespace yandex{namespace intern{namespace sorters
     constexpr std::size_t bucketsSize = static_cast<std::size_t>(1) << blockBitSize;
     constexpr Data mask = static_cast<Data>(fullBlock);
 
+    constexpr std::size_t prefixByteSize = sizeof(Data) - 1;
+    constexpr std::size_t suffixByteSize = sizeof(Data) - prefixByteSize;
+    constexpr std::size_t prefixBitSize = 8 * prefixByteSize;
+    constexpr std::size_t suffixBitSize = 8 * suffixByteSize;
+    constexpr std::size_t prefixSize = std::size_t(1) << prefixBitSize;
+    constexpr std::size_t suffixSize = std::size_t(1) << suffixBitSize;
+
+    constexpr std::size_t prefixTreeSize = prefixSize * 2;
+
     BalancedSplitSorter::BalancedSplitSorter(const boost::filesystem::path& src, const boost::filesystem::path& dst):
         Sorter(src, dst),
         root_(dst.parent_path() / boost::filesystem::unique_path())
@@ -36,85 +45,9 @@ namespace yandex{namespace intern{namespace sorters
 
     void BalancedSplitSorter::sort()
     {
-        constexpr std::size_t prefixByteSize = sizeof(Data) - 1;
-        constexpr std::size_t suffixByteSize = sizeof(Data) - prefixByteSize;
-        constexpr std::size_t prefixBitSize = 8 * prefixByteSize;
-        constexpr std::size_t suffixBitSize = 8 * suffixByteSize;
-        constexpr std::size_t prefixSize = std::size_t(1) << prefixBitSize;
-        constexpr std::size_t suffixSize = std::size_t(1) << suffixBitSize;
-
-        constexpr std::size_t prefixTreeSize = prefixSize * 2;
-
-        std::size_t inputByteSize;
-        std::vector<std::size_t> prefixTree(prefixTreeSize);
-        {
-            detail::SequencedReader input(source());
-            input.setBufferSize(1024 * 1024);
-            inputByteSize = input.size();
-            Data data;
-            while (input.read(data))
-            {
-                const std::size_t key = data >> suffixBitSize;
-                ++prefixTree[key + prefixSize];
-            }
-            if (!input.eof())
-                BOOST_THROW_EXCEPTION(InvalidFileSizeError());
-        }
-        // balance
-        std::vector<bool> isEnd(prefixTreeSize);
-        for (std::size_t i = prefixTreeSize - 1; i >= prefixSize; --i)
-            isEnd[i] = true;
-        for (std::size_t i = prefixSize - 1; i < prefixTreeSize; --i)
-        {
-            const std::size_t left = 2 * i;
-            const std::size_t right = left + 1;
-            if (isEnd[left] && isEnd[right] &&
-#if 0
-                prefixTree[left] < prefixSize && prefixTree[right] < prefixSize)
-#elif 1
-                prefixTree[left] + prefixTree[right] < prefixSize)
-#else
-                prefixTree[left] + prefixTree[right] < 10)
-#endif
-            {
-                isEnd[left] = false;
-                isEnd[right] = false;
-                prefixTree[i] = prefixTree[left] + prefixTree[right];
-                isEnd[i] = true;
-            }
-        }
-        // index structure
-        std::vector<std::size_t> id2prefix;
-        std::unordered_map<std::size_t, std::size_t> prefix2id;
-        const std::function<void (const std::size_t)> dfs =
-            [&](const std::size_t prefix)
-            {
-                if (isEnd[prefix])
-                {
-                    const std::size_t id = id2prefix.size();
-                    id2prefix.push_back(prefix);
-                    prefix2id[prefix] = id;
-                }
-                else
-                {
-                    dfs(prefix * 2);
-                    dfs(prefix * 2 + 1);
-                }
-            };
-        dfs(1);
-#if 0
-        for (std::size_t i = 0; i < prefixTreeSize; ++i)
-        {
-            if (isEnd[i])
-            {
-                const std::size_t id = id2prefix.size();
-                id2prefix.push_back(i);
-                prefix2id[i] = id;
-            }
-        }
-#endif
+        buildPrefixSplit();
         // prepare files
-        std::vector<boost::filesystem::path> part(id2prefix.size());
+        std::vector<boost::filesystem::path> part(id2prefix_.size());
         for (boost::filesystem::path &path: part)
             path = root_ / boost::filesystem::unique_path();
         // split
@@ -126,15 +59,15 @@ namespace yandex{namespace intern{namespace sorters
             {
                 output[i].reset(new detail::SequencedWriter(part[i]));
                 output[i]->setBufferSize(1024 * 1024);
-                output[i]->allocate(sizeof(Data) * prefixTree[id2prefix[i]]);
+                output[i]->allocate(sizeof(Data) * id2size_[i]);
             }
             Data data;
             while (input.read(data))
             {
                 std::size_t prefix = prefixSize + (data >> suffixBitSize);
-                while (!isEnd[prefix])
+                while (!isEnd_[prefix])
                     prefix >>= 1;
-                const std::size_t id = prefix2id.at(prefix);
+                const std::size_t id = prefix2id_.at(prefix);
                 output[id]->write(data);
                 //std::cerr << std::setw(8) << std::setfill('0') << std::hex << data << " -> " << id << ' ' << part[id] << std::endl;
             }
@@ -144,7 +77,7 @@ namespace yandex{namespace intern{namespace sorters
         }
         // merge
         detail::SequencedWriter output(destination());
-        output.allocate(inputByteSize);
+        output.allocate(inputByteSize_);
         for (const boost::filesystem::path &path: part)
         {
             std::vector<Data> data = detail::io::readFromFile(path);
@@ -152,5 +85,80 @@ namespace yandex{namespace intern{namespace sorters
             output.write(data.data(), data.size());
         }
         output.close();
+    }
+
+    void BalancedSplitSorter::buildPrefixSplit()
+    {
+        std::vector<std::size_t> prefixTree(prefixTreeSize);
+        {
+            detail::SequencedReader input(source());
+            input.setBufferSize(1024 * 1024);
+            inputByteSize_ = input.size();
+            Data data;
+            while (input.read(data))
+            {
+                const std::size_t key = data >> suffixBitSize;
+                ++prefixTree[key + prefixSize];
+            }
+            if (!input.eof())
+                BOOST_THROW_EXCEPTION(InvalidFileSizeError());
+        }
+        // balance
+        isEnd_.resize(prefixTreeSize);
+        for (std::size_t i = prefixTreeSize - 1; i >= prefixSize; --i)
+            isEnd_[i] = true;
+        for (std::size_t i = prefixSize - 1; i < prefixTreeSize; --i)
+        {
+            const std::size_t left = 2 * i;
+            const std::size_t right = left + 1;
+            if (isEnd_[left] && isEnd_[right] &&
+#if 0
+                prefixTree[left] < prefixSize && prefixTree[right] < prefixSize)
+#elif 1
+                prefixTree[left] + prefixTree[right] < prefixSize)
+#else
+                prefixTree[left] + prefixTree[right] < 10)
+#endif
+            {
+                isEnd_[left] = false;
+                isEnd_[right] = false;
+                prefixTree[i] = prefixTree[left] + prefixTree[right];
+                isEnd_[i] = true;
+            }
+        }
+        // index structure
+        // FIXME count sort for small suffixes
+        buildCompressedPrefixSplit(1);
+        // save size
+        id2size_.resize(id2prefix_.size());
+        for (std::size_t id = 0; id < id2prefix_.size(); ++id)
+            id2size_[id] = prefixTree[id2prefix_[id]];
+    }
+
+    void BalancedSplitSorter::buildCompressedPrefixSplit(const std::size_t prefix)
+    {
+        // TODO implement non-recursive
+        if (isEnd_[prefix])
+        {
+            const std::size_t id = id2prefix_.size();
+            id2prefix_.push_back(prefix);
+            prefix2id_[prefix] = id;
+        }
+        else
+        {
+            buildCompressedPrefixSplit(prefix * 2);
+            buildCompressedPrefixSplit(prefix * 2 + 1);
+        }
+#if 0
+        for (std::size_t i = 0; i < prefixTreeSize; ++i)
+        {
+            if (isEnd[i])
+            {
+                const std::size_t id = id2prefix.size();
+                id2prefix.push_back(i);
+                prefix2id[i] = id;
+            }
+        }
+#endif
     }
 }}}
