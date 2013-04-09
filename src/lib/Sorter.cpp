3c0a2c9b7c69033e12ae00acbf7fc68b3804a02a
Aleksey Filippov <sarum9in@gmail.com>
2013-04-10 00:11:37 +0400

    sorters: BlockSorter, HalfSplitSorter and MergeSorter were removed.

--- src/lib/Sorter.cpp
+++ src/lib/Sorter.cpp
@@ -1,10 +1,7 @@
 #include "yandex/intern/Sorter.hpp"
 
 #include "yandex/intern/sorters/BalancedSplitSorter.hpp"
-#include "yandex/intern/sorters/BlockSorter.hpp"
-#include "yandex/intern/sorters/HalfSplitSorter.hpp"
 #include "yandex/intern/sorters/InMemorySorter.hpp"
-#include "yandex/intern/sorters/MergeSorter.hpp"
 #include "yandex/intern/sorters/SplitMergeSorter.hpp"
 
 namespace yandex{namespace intern
@@ -12,10 +9,7 @@ namespace yandex{namespace intern
     void Sorter::sort(const boost::filesystem::path &src, const boost::filesystem::path &dst)
     {
         intern::sort<sorters::BalancedSplitSorter>(src, dst);
-        //intern::sort<sorters::BlockSorter>(src, dst);
-        //intern::sort<sorters::HalfSplitSorter>(src, dst);
         //intern::sort<sorters::InMemorySorter>(src, dst);
-        //intern::sort<sorters::MergeSorter>(src, dst);
         //intern::sort<sorters::SplitMergeSorter>(src, dst);
     }
 
